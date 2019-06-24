ff27ec9564520f1b5df367cd484caeb89d910dc6
liuzuolong <1018941664@qq.com>
2019-06-25 01:00:28 +0800

    提交代码

---
+++ src/main/java/com/humous/gp/demo/CommonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.humous.gp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonDemoApplication.class, args);
+    }
+
+}
