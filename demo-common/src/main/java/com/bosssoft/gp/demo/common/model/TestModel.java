768819d197036e397d0e8374591340f99da74a67
liuzuolong <1018941664@qq.com>
2019-06-27 17:32:57 +0800

    add ignorefile

---
+++ demo-common/src/main/java/com/bosssoft/gp/demo/common/model/TestModel.java
@@ -0,0 +1,10 @@
+package com.bosssoft.gp.demo.common.model;
+
+/**
+ * @Author smile7up
+ * @createDate 2019-06-27
+ * @Description
+ */
+
+public class TestModel {
+}
---
+++ demo-web/src/main/java/com/bosssoft/gp/demo/web/CommonDemoApplication.java
@@ -0,0 +1,10 @@
+package com.bosssoft.gp.demo.web;
+
+/**
+ * @Author smile7up
+ * @createDate 2019-06-27
+ * @Description 启动类
+ */
+
+public class CommonDemoApplication {
+}
